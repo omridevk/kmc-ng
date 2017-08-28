003fabb6f95160cc300a3730ab66b9a6a9d8d59a
amirch1 <amir.chervinsky@kaltura.com>
2017-08-28 13:02:23 +0300

    logout logic against the server

--- src/app/components/user-settings/user-settings.component.ts
+++ src/app/components/user-settings/user-settings.component.ts
@@ -3,6 +3,8 @@ import { BrowserService } from 'app-shared/kmc-shell';
 import { AppAuthentication, AppUser, PartnerPackageTypes, AppNavigator } from 'app-shared/kmc-shell';
 import { environment } from 'app-environment';
 import { Md5 } from 'ts-md5/dist/md5';
+import { Http, RequestOptions, Headers } from '@angular/http';
+
 
 @Component({
 	selector: 'kKMCUserSettings',
@@ -15,14 +17,27 @@ export class UserSettingsComponent {
 	public _languages = [{label: "English", value: "en"}, {label: "Deutsch", value: "de"}, {label: "Español", value: "es"}, {label: "Français", value: "fr"}, {label: "日本語", value: "lp"}];
 	public _selectedLanguage = "English";
 
-	constructor(private userAuthentication: AppAuthentication, private appNavigator: AppNavigator, private browserService: BrowserService) {
+	constructor(private userAuthentication: AppAuthentication, private appNavigator: AppNavigator, private browserService: BrowserService, private http: Http) {
 		this._userContext = userAuthentication.appUser;
 	}
 
 	logout() {
+		const ks = this.userAuthentication.appUser.ks;
 		this.userAuthentication.logout();
-		//this.appNavigator.navigateToLogout();
-		document.location.reload();
+
+		let headers = new Headers({ 'Content-Type': 'application/X-www-form-urlencoded' });
+		let options = new RequestOptions({ headers: headers });
+		const logoutUrl = window.location.protocol + '//' + environment.core.kaltura.kmcUrl + "/index.php/kmc/logout";
+		const formBody = "ks=" + ks;
+
+		this.http.post(logoutUrl, formBody, options).subscribe(
+			result => {
+				document.location.reload();
+			},
+			error => {
+				document.location.reload();
+			}
+		);
 	}
 
 	openUserManual() {
