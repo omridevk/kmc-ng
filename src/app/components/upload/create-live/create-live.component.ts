2dd7d05f21f612aa604a9db58b4d25a234fdfc3e
ben.simantov <ben.simantov@kaltura.com>
2017-09-27 16:58:36 +0300

    almost finished kaltura live stream type (need to implement todo's)

--- src/app/components/upload/create-live/create-live.component.ts
+++ src/app/components/upload/create-live/create-live.component.ts
@@ -1,6 +1,8 @@
-import {Component, OnInit, ViewChild} from '@angular/core';
+import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {CreateLiveService, KalturaLiveStream, ManualLive, UniversalLive} from './create-live.service';
-import {AppLocalization} from "@kaltura-ng/kaltura-common";
+import {AppLocalization} from '@kaltura-ng/kaltura-common';
+import {KalturaRecordStatus} from 'kaltura-typescript-client/types/KalturaRecordStatus';
+import {AreaBlockerMessage} from "@kaltura-ng/kaltura-ui";
 
 enum StreamTypes {
   kaltura,
@@ -14,27 +16,28 @@ enum StreamTypes {
   styleUrls: ['./create-live.component.scss'],
   providers: [CreateLiveService]
 })
-export class CreateLiveComponent implements OnInit {
+export class CreateLiveComponent implements OnInit, OnDestroy {
   public _selectedStreamType: StreamTypes = StreamTypes.kaltura;
   public kalturaLiveStreamData: KalturaLiveStream = {
     name: '',
     description: '',
-    transcodingProfile: 'asd1',
+    transcodingProfile: null,
     liveDVR: false,
     enableRecording: false,
-    enableRecordingSelectedOption: 'asd1',
+    enableRecordingSelectedOption: KalturaRecordStatus.appended,
     previewMode: false
   };
   public manualLiveData: ManualLive;
   public universalLiveData: UniversalLive;
   public _availableStreamTypes: Array<{ value: StreamTypes, label: string }>;
   public _streamTypes = StreamTypes;
+  public _blockerState: { isBusy: boolean, message: AreaBlockerMessage } = {isBusy: false, message: null};
 
   @ViewChild('kalturaLiveStreamComponent') kalturaLiveStreamComponent;
   @ViewChild('manualLiveComponent') manualLiveComponent;
   @ViewChild('universalLiveData') universalLiveComponent;
 
-  constructor(createLiveService: CreateLiveService, private _appLocalization: AppLocalization) {
+  constructor(private createLiveService: CreateLiveService, private _appLocalization: AppLocalization) {
   }
 
   ngOnInit() {
@@ -45,11 +48,31 @@ export class CreateLiveComponent implements OnInit {
     ];
   }
 
-  checkValidityOfCurrentSelectedForm() {
-    // if (this._selectedStreamType === 'a') {
-    //   console.log(this.manualLiveComponent.isValid());
-    // } else if (this._selectedStreamType === 'b') {
-    //   console.log(this.kalturaLiveStreamComponent.isValid());
-    // }
+  ngOnDestroy() {
+  }
+
+  submitCurrentSelectedForm() {
+    if (this._selectedStreamType === StreamTypes.kaltura) {
+      if (this.kalturaLiveStreamComponent.validate()) {
+        this.createLiveService.createKalturaLiveStream(this.kalturaLiveStreamData)
+          .cancelOnDestroy(this)
+          .subscribe(response => {
+            // todo:
+            // show  the server returns the new KalturaLiveStreamEntry object. Display a localized question to the user:
+            // title: Stream has been created successfully.
+            // message: Live Stream Entry has been created successfully.\nDo you want to view entry details?
+            // If the user selects "No" - stay in current page.
+            // If the user selects "Yes" - navigate to the new entry page by its ID.
+            // All section leaving validations should apply (verify navigation if there is unsaved data).
+          }, error => {
+            this._updateAreaBlockerState(false, error.message)
+          });
+      }
+    }
+  }
+
+  private _updateAreaBlockerState(isBusy: boolean, message: AreaBlockerMessage): void {
+    this._blockerState.isBusy = isBusy;
+    this._blockerState.message = message;
   }
 }
